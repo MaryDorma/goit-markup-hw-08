00b9fd42b1b1004cae4c154748a0cf3c09ae51d6
MaryDorma <maria.dorma96@gmail.com>
2022-07-18 21:26:07 +0200

    hw #8

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-btn"),
     closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".header-menu"),
+    menu: document.querySelector(".mobile-menu"),
     body: document.querySelector("body"),
   };
   refs.openMenuBtn.addEventListener("click", toggleMenu);
